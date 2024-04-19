0575218882c0301343b14b60cd3d2a941186983b
arsyad <harushard@gmail.com>
2024-04-19 17:01:36 +0800

    add restore and npm install at build and test scripts

--- build.ps1
+++ build.ps1
@@ -1,12 +1,18 @@
+# restore packages
+dotnet restore NumbersToWords.WebApi/NumbersToWords.WebApi.csproj
+
 # publish web api
 dotnet publish NumbersToWords.WebApi/NumbersToWords.WebApi.csproj -c Release -o output
 
 # change to web ui directory
 cd NumbersToWords.WebUi
 
+# install required packages
+npm install
+
 # publish web ui
 npm run build
 
 # copy published web ui to static file's server path
-Copy-Item -Path output\* -Destination ..\output\ui -Recurse
+Copy-Item -Path output\* -Destination ..\output\ui -Recurse -Force
 
--- test.ps1
+++ test.ps1
@@ -1,1 +1,4 @@
+# restore packages
+dotnet restore NumbersToWords.WebApi/NumbersToWords.WebApi.csproj
+
 Start-Process powershell -ArgumentList "-noexit", "-command dotnet test NumbersConverter.UnitTests\NumbersConverter.UnitTests.csproj"
